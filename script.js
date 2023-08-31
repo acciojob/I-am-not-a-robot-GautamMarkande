2e40f706575a3e8542da847c333a76a082434475
GautamMarkande <118103083+GautamMarkande@users.noreply.github.com>
2023-08-31 12:14:03 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,16 +1,41 @@
 //your code here
-const images = Array.from(document.getElementByTagName("img"));
+const images = Array.from(document.getElementsByTagName("img"));
+const main = document.getElementById("main");
+const p = document.createElement("p");
 let count = 0;
 let arr = [];
-const reset = document.getElement1ById("reset");
-const verify = document.getElement1ById("verify");
+const reset = document.getElementById("reset");
+reset.addEventListener("click",()=>{
+	arr=[];
+	reset.style.visibility = "hidden";
+	verify.style.visibility = "hidden";
+	images.forEach(img=>img.style.border="none");
+	p.remove();
+})
+const verify = document.getElementById("verify");
+
+verify.addEventListener("click",()=>{
+	if(arr.length>=2 && arr[1]===arr[0]){
+		
+		p.innerText = "You are a human. Congratulations!"
+		
+	}else{
+		p.innerText = "We can't verify you as a human. You selected the non-identical tiles."
+	}
+	main.appendChild(p);
+	verify.style.visibility = "hidden";
+})
 images.forEach((img)=>{
 	img.addEventListener(("click"),(e)=>{
-		reset.style.visiblity = "visible";
+		console.log(img);
+		img.style.border = "2px solid blue"
+		reset.style.visibility = "visible";
 		count++;
-		arr.push(img.ClassName);
-		if(arr.length>=2 && e.ClassName!==arr[0]){
-			verify.style.visiblity = "visible";
+		arr.push(img.className);
+		console.log(arr);
+		if(arr.length>=2){
+			console.log(arr.length);
+			verify.style.visibility = "visible";
 		}
 	})
 })
